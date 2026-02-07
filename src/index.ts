24ca539327169a93ae3a1a8650d40ceee4ab0d5a
EthanWang706 <ethanbeijing@126.com>
2026-02-07 11:26:02 +0800

    chore: bump version to 1.0.7

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import { isWrappedAvailable } from "./utils/dates";
 import { formatCostFull, formatNumber, formatNumberFull } from "./utils/format";
 import type { CodexStats } from "./types";
 
-const VERSION = "1.0.6";
+const VERSION = "1.0.7";
 
 function printHelp() {
   console.log(`
